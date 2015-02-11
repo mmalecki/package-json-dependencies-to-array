ef7c3b20d53cb2d3062924414ab59e70c573ac26
Maciej Małecki <me@mmalecki.com>
2015-02-11 21:11:38 +0300

    Handle bundled dependencies sanely

--- package-json-dependencies-to-array.js
+++ package-json-dependencies-to-array.js
@@ -2,13 +2,15 @@
 
 module.exports = function(packageJson) {
   var result = []
+  var bundled = packageJson.bundledDependencies || packageJson.bundleDependencies || [];
 
   function process(obj, type) {
     obj && typeof obj === 'object' && Object.keys(obj).forEach(function(package_) {
       result.push({
         package: package_,
         version: obj[package_],
-        type: type
+        type: type,
+        bundled: bundled.indexOf(package_) !== -1
       })
     })
   }
@@ -16,7 +18,18 @@ module.exports = function(packageJson) {
   process(packageJson.dependencies, 'dependency')
   process(packageJson.devDependencies, 'dev')
   process(packageJson.optionalDependencies, 'optional')
-  process(packageJson.bundleDependencies || packageJson.bundledDependencies, 'bundled')
+
+  // Find all the dependencies that are only bundled, and we don't know their
+  // type nor version number (this would only happen in malformed package.json's,
+  // but I'm pretty sure it happens).
+  bundled.forEach(function(bundle) {
+    if (!result.some(function(r) { return r.package === bundle })) {
+      result.push({
+        package: bundle,
+        bundled: true
+      })
+    }
+  })
 
   return result
 }
